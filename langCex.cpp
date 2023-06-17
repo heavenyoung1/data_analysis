1dc8a6ede58810441bbc0218a040b5ae6ab034a9
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2023-06-17 12:43:54 +0300

    Update langCex.cpp

--- langCex.cpp
+++ langCex.cpp
@@ -553,4 +553,26 @@ return 0;
 
 // EX 29; Есть две коробки, первая размером A1×B1×C1, вторая размером A2×B2×C2. Определите, можно ли разместить одну из этих коробок внутри другой, при условии, что поворачивать коробки можно только на 90 градусов вокруг ребер.
 
+#include <iostream>
+using namespace std;
 
+int main() {
+  int a1, b1, c1, a2, b2, c2;
+  cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2;
+  swap(a1, b1);
+  swap (a1, c1);
+  swap (b1, c1);
+  swap(a2, b2);
+  swap (a2, c2);
+  swap (b2, c2);
+  if ((a2 - a1) + (b2 - b1) + (c2 - c1) == 0) {
+    cout << "Boxes are equal";
+  } else if ((a2 - a1) + (b2 - b1) + (c2 - c1) <= 0) {
+    cout << "The first box is larger than the second one"; 
+  } else if ((a2 - a1) + (b2 - b1) + (c2 - c1) >= 0) {
+    cout << "The first box is smaller than the second one";
+  } else {
+      cout << "Boxes are incomparable";
+  }
+  return 0;
+  }
