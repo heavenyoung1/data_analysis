dff4146533627c728796f076a2b2b4a925725318
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2023-06-28 16:19:25 +0300

    Update langCex.cpp

--- langCex.cpp
+++ langCex.cpp
@@ -1088,4 +1088,18 @@ int main() {
 
 // EX 54; Выведите два числа x и y, являющиеся решением этой системы.
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double a, b, c, d, e, f, x, y;
+  cin >> a >> b >> c >> d >> e >> f;
+  x = (e * d - b * f) / (a * d - b * c);
+  y = (a * f - e * c) / (a * d - b * c);
+  cout << x << " " << y;
+}
+
+// EX 55; Даны вещественные числа a, b, c, d, e, f. Решите систему линейных уравнений
+
 
