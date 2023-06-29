e2fb618068e7404ecdb8049d12e322f97091933b
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2023-06-29 09:55:51 +0300

    Update langCex.cpp

--- langCex.cpp
+++ langCex.cpp
@@ -1103,3 +1103,31 @@ int main() {
 // EX 55; Даны вещественные числа a, b, c, d, e, f. Решите систему линейных уравнений
 
 
+
+
+// ЦИКЛ FOR И МАССИВЫ
+
+EX 1; Выведите все элементы массива с четными индексами (то есть A[0], A[2], A[4], ...).
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector <int> a(n);
+    //считывание
+    for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    }
+    for (int i = 0; i <= n - 1; i += 2) {
+      cout << a[i] << " ";
+    }
+
+    return 0;
+}
+
+// EX 2; Выведите все четные элементы массива.
+
