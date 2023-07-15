7fa4446cc1dc3599d231af94b8a2402ce2bd84d5
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2023-07-15 08:58:54 +0300

    Update langCex.cpp

--- langCex.cpp
+++ langCex.cpp
@@ -2188,3 +2188,26 @@ int main() {
 // Решение оформите в виде функции MinDivisor(n). Количество операций в программе должно быть пропорционально корню из n.
 // Указание. Если у числа n нет делителя, меньшего n , то число n — простое и ответом будет само число n.
 
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int MinDivisor(int n) {
+  for (int i = 2, qua = sqrt(n); i <= qua; i++) {
+    if (n % i == 0) {
+      return i;
+    } else if (i == qua) {
+      break;
+    }
+  }
+  return n;
+}
+  
+int main() {
+  int n;
+  cin >> n;
+  cout << MinDivisor(n);
+}
+
+// EX 8; Дано натуральное число n > 1. Проверьте, является ли оно простым. Программа должна вывести слово YES, если число простое и NO, если число составное.
+
